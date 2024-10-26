256fe32f1b3827754efc4342f05c5228350badbc
ahmedootta <ahmedfadl3019@gmail.com>
2024-10-26 21:24:11 +0300

    add both ways of testing 'Postman, HTML form' in signin & signup endpoints

--- app/app.py
+++ app/app.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 from werkzeug.security import check_password_hash, generate_password_hash
 import json
 import datetime
-
 import os
 
 
@@ -26,47 +25,98 @@ client = MongoClient(MONGO_URI, ssl=True, tls=True)
 
 db = client.Ideanest
 members_collection = db.members
+organization_collection = db.organizations
 
 @app.route('/') 
 def index():
     return render_template("index.html")  
 
+# ----------------------------------------------------------
+
 @app.route('/signup', methods=['GET', 'POST'])  
 def signup():
     if request.method == "POST":
-        name = request.form.get('name')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        r_password = request.form.get('r_password')
-        access_level = request.form.get('role')
-        if password == r_password:
-            exist_user = members_collection.find_one({'email': email})
-            if exist_user:
-                flash("User already exists!", 'error')
-                return redirect(url_for('signup'))  
-            else:    
-                new_member = {
-                    'name': name,
-                    'email': email,
-                    'password': generate_password_hash(password),
-                    'access_level': access_level
-                }
-            
-            # members_collection.insertOne(new_member) 
-            flash("You are successfully registered!", 'success')
-            members_collection.insert_one(new_member)
-            return redirect(url_for('signin'))      
-        else:
-            flash("Password-confirmation doesn't match password!", 'error')
-            return redirect(url_for('signup'))   
+        # TEST THROUGH POSTMAN
+        if request.is_json:
+            data = request.get_json()
+            name = data.get('name')
+            email = data.get('email')
+            password = data.get('password')
+            r_password = data.get('r_password')
+            access_level = data.get('role')
+            if password == r_password:
+                exist_user = members_collection.find_one({'email': email})
+                if exist_user:
+                    return "User already exists!"
+
+                else:    
+                    new_member = {
+                        'name': name,
+                        'email': email,
+                        'password': generate_password_hash(password),
+                        'access_level': access_level
+                    }
+                
+                    try:
+                        members_collection.insert_one(new_member)
+                        return("You are successfully registered!", 'success')
+                    except Exception as e:
+                        return("An error occurred while registering: {}".format(str(e)), 'error')
+
+            else:
+                return("Password-confirmation doesn't match password!", 'error')
+        
+# -----------------------------------------------------------------------------------------------------------
+        # TEST THROUGH HTML PAGES "frontend"
+        else:    
+            name = request.form.get('name')
+            email = request.form.get('email')
+            password = request.form.get('password')
+            r_password = request.form.get('r_password')
+            access_level = request.form.get('role')
+            if password == r_password:
+                exist_user = members_collection.find_one({'email': email})
+                if exist_user:
+                    flash("User already exists!", 'error')
+                    return redirect(url_for('signup'))  
+                else:    
+                    new_member = {
+                        'name': name,
+                        'email': email,
+                        'password': generate_password_hash(password),
+                        'access_level': access_level
+                    }
+                
+                    # members_collection.insertOne(new_member) 
+                    try:
+                        members_collection.insert_one(new_member)
+                        flash("You are successfully registered!", 'success')
+                        return redirect(url_for('signin'))  # Redirect to sign-in after successful registration
+                    except Exception as e:
+                        flash("An error occurred while registering: {}".format(str(e)), 'error')
+                        return redirect(url_for('signup')) 
+
+            else:
+                flash("Password-confirmation doesn't match password!", 'error')
+                return redirect(url_for('signup'))   
     else:    
         return render_template("signup.html")  
+    
+# ----------------------------------------------------------------------------------
 
 @app.route('/signin', methods=['GET', 'POST'])  
 def signin():
-    if request.method == "POST":   
-        email = request.form.get('email')
-        password = request.form.get('password')
+
+    if request.method == "POST":
+        # Test in postman through json-format
+        if request.is_json:
+            data = request.get_json()
+            email = data.get('email')
+            password = data.get('password')
+        else:
+            # For form submission through HTML pages
+            email = request.form.get('email')
+            password = request.form.get('password')
 
         # Check if user exists
         user = members_collection.find_one({'email': email})
@@ -107,6 +157,8 @@ def refresh_token():
         "refresh_token": new_refresh_token
     }), 200
           
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)  # Ensure debug is set to True
